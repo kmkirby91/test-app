90fcf3f9d48219778f26db45969c319c0899b99e
kmkirby91 <kmkirby91@gmail.com>
2025-02-09 21:32:48 -0600

    Okay this just has some refreshing page errors but at least adds the task to the session state correctly.

--- newapp.py
+++ newapp.py
@@ -21,11 +21,26 @@ col1, col2, col3 = st.columns(3)
 def update_tasks(column, updated_tasks):
     st.session_state.tasks[column] = updated_tasks
 
+# Add new task using a form
+st.subheader("Add Task")
+with st.form(key="add_task_form"):
+    new_task = st.text_input("Task Name")
+    category = st.selectbox("Category", ["To Do", "In Progress", "Done"])
+    submit_button = st.form_submit_button(label="Add Task")
+    
+    if submit_button and new_task.strip():
+        st.session_state.tasks[category].append(new_task.strip())
+
 # Kanban Columns
 with col1:
     st.subheader("To Do")
     updated_todo = sort_items(st.session_state.tasks["To Do"], key="todo")
     update_tasks("To Do", updated_todo)
+    pressme = st.button(label="PRESSME")
+#    if pressme:
+#        st.session_state.tasks["To Do"].append("hellooooo")
+#        updated_todo = sort_items(st.session_state.tasks["To Do"], key="todo")
+#        update_tasks("To Do", updated_todo)
 
 with col2:
     st.subheader("In Progress")
@@ -37,11 +52,13 @@ with col3:
     updated_done = sort_items(st.session_state.tasks["Done"], key="done")
     update_tasks("Done", updated_done)
 
-# Add new task
-st.sidebar.header("Add Task")
-new_task = st.sidebar.text_input("Task Name")
-category = st.sidebar.selectbox("Category", ["To Do", "In Progress", "Done"])
-if st.sidebar.button("Add Task"):
-    if new_task:
-        st.session_state.tasks[category].append(new_task)
-        st.rerun()
+with col1:
+        updated_todo2 = sort_items(st.session_state.tasks["To Do"], key="todo2")
+        update_tasks("To Do", updated_todo2)
+
+
+pressme2 = st.button(label="PRESSME2")
+#        st.rerun()
+#        updated_todo2 = sort_items(st.session_state.tasks["To Do"], key="todo2")
+#        update_tasks("To Do", updated_todo2)
+# 2/9/25 button adds to session state but page doesn't refresh
